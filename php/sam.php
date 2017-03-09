8157fc4b5184f54b3927a94b89a0b0db160cbe9f
Sundar Saravanan <sundar@Sundars-MacBook-Air.local>
2017-03-09 19:52:22 +0530

    upload

--- php/sam.php
+++ php/sam.php
@@ -10,14 +10,21 @@
     $file_ext=explode(".",$file_name);
     $file_ext=strtolower(end($file_ext));
     $allowed=array('jpeg','jpg','png');
+    if($_FILES['uploadedfile']['error'] == 2) {
+       echo "You've exceeded the maximum file upload size of 512kb.";
+       return false;
+     }
     if(in_array($file_ext,$allowed)){
       if($file_error===0){
         if($file_size<=2097152){
-$file_new_name=uniqid('',true).'.'.$file_ext;
-echo $file_destination=$_SERVER['DOCUMENT_ROOT'] .'/uploads/'.$file_new_name;
+          $file_new_name=uniqid('',true).'.'.$file_ext;
+          echo $file_destination='uploads/'.$file_new_name;
 
-if(move_uploaded_file($file_tmp,$file_destination)){
+          if(move_uploaded_file($file_tmp,$file_destination)){
   echo $file_destination;
+}
+else {
+  echo "failed";
 }
         }
       }
